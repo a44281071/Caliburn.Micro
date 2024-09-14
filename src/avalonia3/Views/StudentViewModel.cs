365b7be5900f1ac3f141038f5c6c94dbe04df44b
SamTi（三台） <a44281071@sina.com>
2024-09-14 13:54:27 +0800

    test CM conventions and attached.

--- src/avalonia3/Views/StudentViewModel.cs
+++ src/avalonia3/Views/StudentViewModel.cs
@@ -6,6 +6,19 @@ using System.Threading.Tasks;
 using Caliburn.Micro;
 
 namespace Avalonia3;
+
 public class StudentViewModel : Screen
 {
+    private string? stuName;
+
+    public string? StuName { get => stuName; set => Set(ref stuName, value); }
+
+    public async Task RandomName()
+    {
+        await Task.Delay(100);
+        StuName = Random.Shared.NextDouble().ToString();
+    }
+
+    public void ClearName() =>
+        StuName = "";
 }
